a0bf8d4d8ad79e320b9ca1cefca7cf08fe7455c7
Diwa Hanugra Putra <diwahp@gmail.com>
2023-01-18 19:42:39 +0700

    Create vnc-ubuntu.sh

---
+++ vnc-ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#update repo ubuntu
+apt-get update -y
+#install xfce dan tigervnc
+apt-get install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils tigervnc-standalone-server tigervnc-common -y
+
+#belom kelar
