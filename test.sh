e4da4dbbcc7b11b2fdc6247355f10cec597625f3
Diwa Hanugra Putra <diwahp@gmail.com>
2023-02-03 02:31:26 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Please input your apache2 port : " phttp
+
+ufw reset
+ufw allow 232/tcp
+ufw allow 161/udp
+ufw allow from 103.18.133.200 to any port $phttp
+ufw allow from 103.18.133.200 to any port 3306
+ufw allow from 103.18.132.207 to any port $phttp
+ufw allow from 103.18.132.207 to any port 3306
+ufw reload
+ufw enable
